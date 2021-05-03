476c388589254e370bf0410f45bf10f04007e2c6
Peter Bücker <peter.buecker@form3.tech>
2021-05-03 16:22:02 +0200

    Add retries

--- internal/api/portal/client.go
+++ internal/api/portal/client.go
@@ -1,11 +1,16 @@
 package portal
 
 import (
+	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/go-resty/resty/v2"
+)
+
+const (
+	grafanaStarting = "Your instance is starting"
 )
 
 type Client struct {
@@ -25,7 +30,10 @@ func NewClient(baseURL, apiKey string, opts ...ClientOpt) (*Client, error) {
 		SetDebug(len(os.Getenv("HTTP_DEBUG")) != 0).
 		SetAuthToken(apiKey).
 		SetHostURL(url).
-		SetTimeout(30 * time.Second)
+		SetTimeout(30 * time.Second).
+		SetRetryWaitTime(10 * time.Second).
+		SetRetryCount(6).
+		AddRetryCondition(CanRetry)
 
 	c := &Client{
 		client: resty,
@@ -42,4 +50,12 @@ func WithUserAgent(userAgent string) ClientOpt {
 	return func(c *Client) {
 		c.client.SetHeader("User-Agent", userAgent)
 	}
+}
+
+// We retry for two reasons:
+// 1. Grafana Cloud APIs might apply rate limiting to API requests
+// 2. Newly created Grafana Cloud Stacks don't accept requests to create Grafana API keys immediately
+func CanRetry(r *resty.Response, err error) bool {
+	return r.StatusCode() == http.StatusTooManyRequests ||
+		strings.Contains(r.String(), grafanaStarting)
 }
